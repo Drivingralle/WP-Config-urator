5ef5d4b521cd2aeb97736bc99368e8c1d0c2d784
Ralf Wiechers <ralf@cross-media-cloud.de>
2022-10-04 13:14:21 +0200

    better plugin header

--- wp-config-urator.php
+++ wp-config-urator.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * Plugin Name: WP Config-urator
- * Description: Plugin to allow configuration of WordPress via wp-config.
- * Version:     1.0.0
- * Author:      Cross Media Cloud
- * Author URI:  https://www.cross-media-cloud.de
- * License:     GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: wp-config-urator
- * Domain Path: /languages/
+ * Plugin Name:       WP Config-urator
+ * Description:       Plugin to allow configuration of WordPress via wp-config.
+ * Version:           1.0.0
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Author:            Cross Media Cloud
+ * Author URI:        https://www.cross-media-cloud.de
+ * License:           GNU General Public License v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-config-urator
+ * Domain Path:       /languages/
  */
 
  if( ! defined( 'ABSPATH' ) ) {
