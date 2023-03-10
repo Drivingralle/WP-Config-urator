9d6c878642b58ed9da36816f5344bc71c9fcc10b
Ralf Wiechers <Drivingralle@users.noreply.github.com>
2023-03-10 10:16:40 +0100

    Increase requirment to PHP 8.0

--- wp-config-urator.php
+++ wp-config-urator.php
@@ -4,9 +4,9 @@
  * Description:       Plugin to allow configuration of WordPress via wp-config.
  * Version:           1.0.0
  * Requires at least: 6.1
- * Requires PHP:      7.0
- * Author:            Cross Media Cloud
- * Author URI:        https://www.cross-media-cloud.de
+ * Requires PHP:      8.0
+ * Author:            Drivingralle
+ * Author URI:        https://www.drivingralle.de
  * License:           GNU General Public License v3 or later
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -16,17 +16,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * @param mixed $pre_option   The value to return instead of the option value. This differs
+ * Filter every request for get_options.
+ *
+ * @param mixed $pre_option The value to return instead of the option value. This differs
  *                            from `$default`, which is used as the fallback value in the event
  *                            the option doesn't exist elsewhere in get_option().
  *                            Default false (to skip past the short-circuit).
  * @param string $option_name Name of the option.
- * @param mixed $default      The fallback value to return if the option does not exist.
+ * @param mixed $default The fallback value to return if the option does not exist.
  *                            Default false.
  *
  * @return mixed
  */
-function wp_config_urator_get_option( $pre_option, string $option_name, $default ) {
+function wp_config_urator_get_option( mixed $pre_option, string $option_name, mixed $default ): mixed {
 
 	// Convert option name to name of CONSTANT
 	$constant_name = str_replace( '-', '_', strtoupper( $option_name ) );
@@ -41,6 +43,8 @@ function wp_config_urator_get_option( $pre_option, string $option_name, $default
 	 *
 	 * Can return value directly because check for existence of constant was done earlier.
 	 */
+
 	return constant( $constant_name );
 }
+
 add_filter( 'pre_option', 'wp_config_urator_get_option', 999, 3 );
